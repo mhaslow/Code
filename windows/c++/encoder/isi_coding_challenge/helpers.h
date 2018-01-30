d9099aedbf9cb3c6c3e73e6de20b3cf7cd0e45f3
Michael Haslow <michael.haslow@gmail.com>
2018-01-29 22:58:44 -0800

    Renamed the project and upgraded the toolset and framework to run off of VS2017 instead of VS2015

--- windows/c++/encoder/isi_coding_challenge/helpers.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <sys\stat.h>
-
-// Function will return back True or False depending on whether the path exists
-bool does_file_exist(char *file_path);
-
-void safe_delete(void **p);
